dafdc6883ec5e58e88d1476ca31f54f4e0a2ccba
aniketbhabat <aniketbhabat7050@gmail.com>
2024-05-30 23:45:45 +0530

    done

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+function Hero() {
+    return (
+        <div className="hero">
+            <div className="hero-inner">
+                <h1 className="black">Let us find your</h1>
+                <h1>Forever Food</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                <p>Nesciunt illo tenetur fuga ducimus numquam ea!</p>
+                <div>
+                    <button className="pink">Search Now</button>
+                    <button className="white">Know More</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
